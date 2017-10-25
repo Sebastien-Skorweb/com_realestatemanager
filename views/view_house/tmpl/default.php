7442cea146a02410835a2aaab0fd3b175a277110
Sebastien <sebastien.gamarde@skorweb.fr>
2017-10-25 17:33:30 +0200

    Ajustement du datepicker en français, et commençant au lundi.

--- views/view_house/tmpl/default.php
+++ views/view_house/tmpl/default.php
@@ -29,6 +29,26 @@ if (!JFactory::getApplication()->get('os_lightbox'))
   $doc->addScript("//maps.googleapis.com/maps/api/js?$api_key");
   if($realestatemanager_configuration['street_view']['show']){
     ?>
+    <script type="text/javascript">
+     jQuerREL.datepicker.regional['fr'] = {clearText: 'Effacer', clearStatus: '',
+      closeText: 'Fermer', closeStatus: 'Fermer sans modifier',
+      prevText: '<Préc', prevStatus: 'Voir le mois précédent',
+      nextText: 'Suiv>', nextStatus: 'Voir le mois suivant',
+      currentText: 'Courant', currentStatus: 'Voir le mois courant',
+      monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin',
+      'Juillet','Août','Septembre','Octobre','Novembre','Décembre'],
+      monthNamesShort: ['Jan','Fév','Mar','Avr','Mai','Jun',
+      'Jul','Aoû','Sep','Oct','Nov','Déc'],
+      monthStatus: 'Voir un autre mois', yearStatus: 'Voir un autre année',
+      weekHeader: 'Sm', weekStatus: '',
+      dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
+      dayNamesShort: ['Dim','Lun','Mar','Mer','Jeu','Ven','Sam'],
+      dayNamesMin: ['Di','Lu','Ma','Me','Je','Ve','Sa'],
+      dayStatus: 'Utiliser DD comme premier jour de la semaine', dateStatus: 'Choisir le DD, MM d',
+      dateFormat: 'dd/mm/yy', firstDay: 1, 
+      initStatus: 'Choisir la date', isRTL: false};
+     jQuerREL.datepicker.setDefaults(jQuerREL.datepicker.regional['fr']);
+    </script>
     <script type="text/javascript">  
       window.addEvent('domready', function() {
         initialize();
